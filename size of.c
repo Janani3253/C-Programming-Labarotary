8789027f6327a881fdb93a265b18fbe0d8df6349
Janani3253 <mahendranjanani57@gmail.com>
2024-09-30 15:05:27 +0530

    Create size of.c

---
+++ size of.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+   int a;
+   float b;
+   double c;
+   char d;
+   printf("size of integer is %ld\n",sizeof(a));
+   printf("size of float is %ld\n",sizeof(b));
+   printf("size of double is %ld\n",sizeof(c));
+   printf("size of character is %ld",sizeof(d));
+}
