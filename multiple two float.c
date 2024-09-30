636c76dc909ab096487588e6ffade25bd40bdd4e
Janani3253 <mahendranjanani57@gmail.com>
2024-09-30 14:50:42 +0530

    Create multiple two float.c

---
+++ multiple two float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1,num2,result;
+    printf("Enter num1:");
+    scanf("%f",&num1);
+    printf("Enter num2:");
+    scanf("%f",&num2);
+    result=num1*num2;
+    printf("The result is %.2f",result);
+    return 0;
+}
